eb963b66a93fa943eeb3b937558c76ee9dbd05d3
Jordi Simon <72779969+jordisl@users.noreply.github.com>
2020-10-14 23:08:09 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,17 @@
 basic.forever(function () {
     if (input.buttonIsPressed(Button.A)) {
         pins.digitalWritePin(DigitalPin.P0, 1)
+    } else {
+        pins.digitalWritePin(DigitalPin.P0, 0)
+    }
+    if (input.buttonIsPressed(Button.B)) {
+        pins.digitalWritePin(DigitalPin.P1, 1)
+    } else {
+        pins.digitalWritePin(DigitalPin.P1, 0)
+    }
+    if (input.buttonIsPressed(Button.AB)) {
+        pins.digitalWritePin(DigitalPin.P2, 1)
+    } else {
+        pins.digitalWritePin(DigitalPin.P2, 0)
     }
 })
